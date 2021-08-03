8a1714b7d5fc5272e91b5f37a88ab6d3a298b434
carl-chua <carlchua0701@gmail.com>
2021-08-04 01:08:54 +0800

    Set up w/ firebase

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyATZApxZosMiYGnEPGp5nGB3ereWWxl84w",
+  authDomain: "citi-hackoverflow-group-12.firebaseapp.com",
+  projectId: "citi-hackoverflow-group-12",
+  storageBucket: "citi-hackoverflow-group-12.appspot.com",
+  messagingSenderId: "627690431375",
+  appId: "1:627690431375:web:e54e090c981dc4e6a2a1fa",
+  measurementId: "G-ZGYJQPYTMP"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
