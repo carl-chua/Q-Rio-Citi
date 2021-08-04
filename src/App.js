33eafdccaca2e93cf1254319e03cbd3fe0b708c6
carl-chua <carlchua0701@gmail.com>
2021-08-04 23:16:45 +0800

    Enter Amount

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './Pages/Home';
+import EnterAmount from './Pages/Merchant/EnterAmount';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/customer' component={Home} />
-        <Route exact path='/merchant' component={Home} />
-        <Route exact path='/merchant/enterAmount' component={Home} />
-        <Route exact path='/merchant/qr' component={Home} />
-        <Route exact path='/merchant/success' component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/customer" component={Home} />
+        <Route exact path="/merchant" component={Home} />
+        <Route exact path="/merchant/enterAmount" component={EnterAmount} />
+        <Route exact path="/merchant/qr" component={Home} />
+        <Route exact path="/merchant/success" component={Home} />
       </Switch>
     </div>
   );
--- src/pages/Merchant/EnterAmount.js
+++ src/pages/Merchant/EnterAmount.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
+import { useState } from 'react';
+import { QRCode } from 'react-qrcode-logo';
+
+import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
 
 export default function EnterAmount() {
-    return (
-        <Paper>
+  const [amount, setAmount] = useState();
+  const [qrString, setQRString] = useState();
 
-        </Paper>
-    )
+  const handleChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid item xs={10}>
+        <QRCode
+          value="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+          logoImage="/citi.jpg"
+          logoWidth={40}
+          logoHeight={40}
+        />
+      </Grid>
+      <Grid item xs={10} style={{ marginTop: '30px' }}>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="standard-adornment-amount">
+            Enter Amount:
+          </InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            value={amount}
+            onChange={handleChange}
+            startAdornment={
+              <InputAdornment position="start">
+                <b>S$</b>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
 }
